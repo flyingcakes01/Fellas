2c3c5ef191775b1475a702a6972e5fa59c3b9b51
Sai Aarush Sharma Annapantula <137160728+flyingcakes01@users.noreply.github.com>
2024-08-16 09:58:27 +1000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+  <title>Document</title>
+</head>
+<body>
+
+<?php
+//If your Discord.php file is in another folder you need to change the file path!
+include("https://discord.com/api/webhooks/1273792287440109629/tDCVoehcrjuJPtxL8kd63coelw-V-9eiFUJAYyPZcRI30daeLZPss8-P_IGHNvohzxLe");
+$sendembed = New Discord();
+
+//Executes the function
+$sendembed->Visitor();
+?>
+
+</body>
+</html>
+
